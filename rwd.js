39a5a47cf9e3a3aeb1e78d6c82c3888b153ae473
makoto <makoto@gmail.com>
2022-08-09 18:48:32 +0800

    720px選單5em問題待改善

---
+++ rwd.js
@@ -0,0 +1,17 @@
+// RWD樣式切換
+let title = document.querySelector('header h1');
+let ul = document.querySelector('ul');
+let header = document.querySelector('header');
+
+if(document.body.offsetWidth <= 1024) {
+  title.textContent = '朗智科技';
+}
+
+if(document.body.offsetWidth <= 720) {
+  ul.addEventListener('click', () => {
+    let stretchBlock = document.getElementById('stretchBlock');
+    let stretchDisplay = stretchBlock.style.display;
+    stretchBlock.classList.add('change');
+    stretchBlock.style.display = stretchDisplay === "none" ? 'flex' : 'none';
+  })
+}
