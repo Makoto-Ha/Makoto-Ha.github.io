882b85607a9a02b150a9b8d26507cf52a1f413cc
makoto <makoto@gmail.com>
2022-08-26 17:17:52 +0800

    用CSS新增720px選單伸縮動畫

--- rwd.js
+++ rwd.js
@@ -6,12 +6,3 @@ let header = document.querySelector('header');
 if(document.body.offsetWidth <= 1024) {
   title.innerHTML = '<a href="/index.html">朗智科技</a>';
 }
-
-if(document.body.offsetWidth <= 720) {
-  ul.addEventListener('click', () => {
-    let stretchBlock = document.getElementById('stretchBlock');
-    let stretchDisplay = stretchBlock.style.display;
-    stretchBlock.classList.add('change');
-    stretchBlock.style.display = stretchDisplay === "none" ? 'flex' : 'none';
-  })
-}
